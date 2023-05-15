a9a5f3c14264bb7ad9aca77e85247db44ce4cfa0
André Pereira <andrepereira97@yahoo.com>
2023-05-15 19:27:54 -0300

    :card_file_box: add balance column, transactions table, trigger when update balance

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -18,3 +18,26 @@ CREATE TABLE IF NOT EXISTS `cards` (
     PRIMARY KEY(`id`)
 );
 
+ALTER TABLE `cards` ADD COLUMN `balance` FLOAT NOT NULL DEFAULT 0;
+
+CREATE TABLE transactions (
+    `id` INT AUTO_INCREMENT,
+    `card_id` INT NOT NULL,
+    `amount` FLOAT NOT NULL,
+    `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY(`card_id`) REFERENCES `cards`(`id`)
+)
+
+DELIMITER $$
+
+CREATE TRIGGER update_card_balance_trigger
+AFTER UPDATE ON `cards` FOR EACH ROW
+BEGIN
+  IF NEW.balance != OLD.balance THEN
+    INSERT INTO transactions (card_id, amount) VALUES (NEW.id, NEW.balance - OLD.balance);
+  END IF;
+END$$
+
+DELIMITER ;
+
