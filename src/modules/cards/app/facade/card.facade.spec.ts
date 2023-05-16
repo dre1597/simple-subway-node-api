67b397d52c65c43610555450c23c2f5fcc7d8139
André Pereira <andrepereira97@yahoo.com>
2023-05-16 14:28:23 -0300

    :white_check_mark: add update card tests

--- src/modules/cards/app/facade/card.facade.spec.ts
+++ src/modules/cards/app/facade/card.facade.spec.ts
@@ -1,25 +1,63 @@
 import { describe, expect, it } from 'vitest';
 
-import { CardFacadeInterface } from './card.facade.interface';
 import { CardInMemoryRepository } from '../../infra/repository/in-memory/card.in-memory.repository';
 import { CardFacade } from './card.facade';
 import { AddCardUseCase } from '../use-case/add/add-card.use-case';
+import { UpdateCardUseCase } from '../use-case/update/update-card.use-case';
+import { Card } from '../../domain/card';
+import { NotFoundException } from '../../../@shared/exception/not-found.exception';
 
-const makeSut = (): CardFacadeInterface => {
+const makeSut = () => {
   const repository = new CardInMemoryRepository();
 
   const addUseCase = new AddCardUseCase(repository);
-  return new CardFacade(addUseCase);
+  const updateUseCase = new UpdateCardUseCase(repository);
+
+  const facade = new CardFacade(addUseCase, updateUseCase);
+
+  return { facade, repository };
 };
 
 describe('CardFacade', () => {
   it('should add a card', async () => {
-    const facade = makeSut();
+    const { facade } = makeSut();
 
     const input = {
       name: 'any_name',
     };
 
     await expect(async () => await facade.add(input)).not.toThrow();
+  });
+
+  it('should update a card', async () => {
+    const { facade, repository } = makeSut();
+
+    const card = new Card({
+      name: 'any_name',
+    });
+
+    await repository.save({
+      card,
+    });
+
+    const input = {
+      id: card.id,
+      name: 'updated_name',
+    };
+
+    await expect(async () => await facade.update(input)).not.toThrow();
+  });
+
+  it('should throw NotFoundError when card not found on update', async () => {
+    const { facade, repository } = makeSut();
+
+    const input = {
+      id: 1,
+      name: 'updated_name',
+    };
+
+    await expect(async () => await facade.update(input)).rejects.toThrow(
+      NotFoundException,
+    );
   });
 });
