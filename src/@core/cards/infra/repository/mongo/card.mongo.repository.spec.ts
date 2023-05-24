ff8aa13540773c7160eb77965803c6e2ac5be649
André Pereira <andrepereira97@yahoo.com>
2023-05-24 17:28:22 -0300

    :sparkles: add card mongo repository

---
+++ src/@core/cards/infra/repository/mongo/card.mongo.repository.spec.ts
@@ -0,0 +1,185 @@
+import { MongoHelper } from '../../../../@shared/infra/db/mongo/mongo-helper';
+import { Card, CreateCardInput } from '../../../domain/card';
+import { CardMongoRepository } from './card.mongo.repository';
+import { NotFoundException } from '../../../../@shared/exception/not-found.exception';
+
+const makeSut = () => new CardMongoRepository();
+
+describe('CardMongoRepository', () => {
+  const truncateTables = async () => {
+    const cardsCollection = await MongoHelper.getCollection('cards');
+
+    await cardsCollection.deleteMany({});
+
+    const transactionsCollection = await MongoHelper.getCollection(
+      'transactions',
+    );
+
+    await transactionsCollection.deleteMany({});
+  };
+
+  beforeEach(async () => {
+    await truncateTables();
+  });
+
+  afterEach(async () => {
+    await truncateTables();
+  });
+
+  it('should insert a card', async () => {
+    const repository = makeSut();
+
+    const props: CreateCardInput = {
+      name: 'any_name1',
+    };
+
+    let card = new Card(props);
+
+    const { card: cardInserted } = await repository.save({
+      card,
+    });
+
+    expect(cardInserted.id).toBe(1);
+    expect(cardInserted.name).toBe(props.name);
+    expect(cardInserted.balance).toBe(0);
+
+    props.name = 'any_name2';
+    props.balance = 10;
+
+    card = new Card(props);
+
+    const { card: cardInserted2 } = await repository.save({
+      card,
+    });
+
+    expect(cardInserted2.id).toBe(2);
+    expect(cardInserted2.name).toBe(props.name);
+    expect(cardInserted2.balance).toBe(10);
+  });
+
+  it('should update a card', async () => {
+    const repository = makeSut();
+
+    const props: CreateCardInput = {
+      name: 'any_name',
+    };
+
+    const card = new Card(props);
+
+    const { card: cardInserted } = await repository.save({
+      card,
+    });
+
+    expect(cardInserted.id).toBe(1);
+    expect(cardInserted.name).toBe(props.name);
+    expect(cardInserted.balance).toBe(0);
+
+    cardInserted.update({
+      name: 'updated_name',
+      balance: 10,
+    });
+
+    const { card: cardUpdated } = await repository.save({
+      card: cardInserted,
+    });
+
+    expect(cardUpdated.id).toBe(1);
+    expect(cardUpdated.name).toBe('updated_name');
+    expect(cardUpdated.balance).toBe(10);
+  });
+
+  it('should find a card by id', async () => {
+    const repository = makeSut();
+
+    const props: CreateCardInput = {
+      name: 'any_name',
+      balance: 10,
+    };
+
+    const card = new Card(props);
+
+    const { card: cardInserted } = await repository.save({
+      card,
+    });
+
+    const { card: cardFound } = await repository.findById({
+      id: cardInserted.id,
+    });
+
+    expect(cardFound.id).toBe(cardInserted.id);
+    expect(cardFound.name).toBe(cardInserted.name);
+    expect(cardFound.balance).toBe(cardInserted.balance);
+  });
+
+  it('should throw if card not found', async () => {
+    const repository = makeSut();
+
+    await expect(async () => {
+      await repository.findById({
+        id: 1,
+      });
+    }).rejects.toThrowError(
+      new NotFoundException('card', 'Card with id 1 not found'),
+    );
+  });
+
+  it('should create a transaction when update the balance of a card', async () => {
+    const repository = makeSut();
+
+    const props: CreateCardInput = {
+      name: 'any_name',
+    };
+
+    const card = new Card(props);
+
+    const { card: cardInserted } = await repository.save({
+      card,
+    });
+
+    expect(cardInserted.id).toBe(1);
+    expect(cardInserted.name).toBe(props.name);
+    expect(cardInserted.balance).toBe(0);
+
+    cardInserted.update({
+      name: 'updated_name',
+      balance: 10,
+    });
+
+    const { card: cardUpdated } = await repository.save({
+      card: cardInserted,
+    });
+
+    expect(cardUpdated.id).toBe(1);
+    expect(cardUpdated.name).toBe('updated_name');
+    expect(cardUpdated.balance).toBe(10);
+
+    const { transactions } = await repository.findTransactionsByCardId({
+      cardId: cardUpdated.id,
+    });
+
+    expect(transactions).toHaveLength(1);
+    expect(transactions[0].id).toBe(1);
+    expect(transactions[0].card.id).toBe(cardUpdated.id);
+    expect(transactions[0].amount).toBe(10);
+    expect(transactions[0].timestamp).toBeDefined();
+
+    cardUpdated.update({
+      balance: -10,
+    });
+
+    const { card: cardUpdated2 } = await repository.save({
+      card: cardUpdated,
+    });
+
+    const { transactions: transactionsUpdated } =
+      await repository.findTransactionsByCardId({
+        cardId: cardUpdated2.id,
+      });
+
+    expect(transactionsUpdated).toHaveLength(2);
+    expect(transactionsUpdated[1].id).toBe(2);
+    expect(transactionsUpdated[1].card.id).toBe(cardUpdated.id);
+    expect(transactionsUpdated[1].amount).toBe(-20);
+    expect(transactionsUpdated[1].timestamp).toBeDefined();
+  });
+});
---
+++ src/@core/cards/infra/repository/mongo/card.mongo.repository.ts
@@ -0,0 +1,131 @@
+import {
+  CardRepository,
+  FindCardByIdInputDto,
+  FindCardByIdOutputDto,
+  FindTransactionsByCardIdInputDto,
+  FindTransactionsByCardIdOutputDto,
+  SaveCardInputDto,
+  SaveCardOutputDto,
+} from '../../../domain/card.repository';
+import { MongoHelper } from '../../../../@shared/infra/db/mongo/mongo-helper';
+import { Collection } from 'mongodb';
+import { Card } from '../../../domain/card';
+import { NotFoundException } from '../../../../@shared/exception/not-found.exception';
+import { Transaction } from '../../../domain/transaction';
+
+export class CardMongoRepository implements CardRepository {
+  public async save(input: SaveCardInputDto): Promise<SaveCardOutputDto> {
+    const collection = await this.getCollection();
+
+    if (!input.card.id) {
+      input.card.id = await this._getNextId();
+
+      await collection.insertOne({
+        cardId: input.card.id,
+        name: input.card.name,
+        balance: input.card.balance || 0,
+      });
+    } else {
+      const { card } = await this.findById({
+        id: input.card.id,
+      });
+
+      await this._addTransaction(input.card, input.card.balance, card.balance);
+
+      await collection.updateOne(
+        { cardId: input.card.id },
+        { $set: { name: input.card.name, balance: input.card.balance } },
+      );
+    }
+
+    return {
+      card: new Card({
+        id: input.card.id,
+        name: input.card.name,
+        balance: input.card.balance,
+      }),
+    };
+  }
+
+  public async findById(
+    input: FindCardByIdInputDto,
+  ): Promise<FindCardByIdOutputDto> {
+    const collection = await this.getCollection();
+
+    const card = await collection.findOne({ cardId: input.id });
+
+    if (!card) {
+      throw new NotFoundException('card', `Card with id ${input.id} not found`);
+    }
+
+    return {
+      card: new Card({
+        id: card.cardId,
+        name: card.name,
+        balance: card.balance,
+      }),
+    };
+  }
+
+  public async findTransactionsByCardId(
+    input: FindTransactionsByCardIdInputDto,
+  ): Promise<FindTransactionsByCardIdOutputDto> {
+    const collection = await this.getCollection('transactions');
+
+    const transactions = await collection
+      .find({
+        cardId: input.cardId,
+      })
+      .toArray();
+
+    return {
+      transactions: transactions.map(
+        (transaction) =>
+          new Transaction({
+            id: transaction.transactionId,
+            card: new Card({
+              id: transaction.cardId,
+              name: transaction.cardName,
+              balance: transaction.cardBalance,
+            }),
+            amount: transaction.amount,
+            timestamp: transaction.timestamp,
+          }),
+      ),
+    };
+  }
+
+  private async getCollection(name = 'cards'): Promise<Collection> {
+    return MongoHelper.getCollection(name);
+  }
+
+  private async _getNextId(name = 'cards'): Promise<number> {
+    const collection = await this.getCollection(name);
+
+    return (await collection.countDocuments()) + 1;
+  }
+
+  private async _addTransaction(
+    card: Card,
+    newBalance: number,
+    oldBalance: number,
+  ): Promise<void> {
+    const transactionCollection = await this.getCollection('transactions');
+
+    const transaction = new Transaction({
+      card,
+      id: await this._getNextId('transactions'),
+      amount: newBalance - oldBalance,
+      timestamp: new Date(),
+    });
+
+    await transactionCollection.insertOne({
+      cardId: transaction.card.id,
+      cardName: transaction.card.name,
+      cardBalance: transaction.card.balance,
+      amount: transaction.amount,
+      timestamp: transaction.timestamp,
+      transactionId: transaction.id,
+    });
+  }
+}
